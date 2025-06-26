eddbcb188fd17b602548ffc58bef1ea7f96c9377
yw <yw@1048359499@qq.com>
2025-06-26 19:01:30 +0800

    first commit

--- cmd/kit/main.go
+++ cmd/kit/main.go
@@ -20,7 +20,7 @@ import (
 
 // go build -ldflags "-X main.Version=x.y.z"
 var (
-	// Name is the name of the compiled software.
+	// Name is the name of the compiled software.2
 	Name string
 	// Version is the version of the compiled software.
 	Version string
